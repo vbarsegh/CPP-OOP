4c584757ae0799b819c459249148233e81e2e793
Vardan Barseghyan <vbarsegh@c2r3s5.42yerevan.am>
2024-09-19 14:44:52 +0400

    ex04

--- C++/C01/ex03/main.cpp
+++ C++/C01/ex03/main.cpp
@@ -16,7 +16,7 @@ int main()
     {
     Weapon club = Weapon("crude spiked club");
     HumanB jim("Jim");
-    jim.setWeapon(&club);//bayc ira tvacov aranc &-ia club@???????
+    jim.setWeapon(club);
     jim.attack();
     club.setType("some other type of club");
     jim.attack();
---
+++ C++/C01/ex04/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int	strlen(char *str)
+{
+	int len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+std::string foo(std::string line, char *s1, char *s2)
+{
+	int	ind;
+	int	len_s1;
+
+	while (1)
+	{
+		ind = line.find(s1);
+		if (ind == -1)
+			break ;
+		len_s1 = strlen(s1);
+		line.erase(ind, len_s1);
+		line.insert(ind, s2);
+
+	}
+	return (line);
+}
+
+int main(int argc, char **argv)
+{
+	std::string name_file = argv[1];
+
+    if (argc != 4)
+		return (std::cout << "Wrong input!" << std::endl && 1);
+	std::ifstream cur_file;
+	cur_file.open(argv[1]);
+	if (!cur_file.is_open())
+		return (std::cout << "Cannot open file" << std::endl && 1);
+	std::string	line_in_file;
+	std::string	poxvac_line;
+	std::ofstream final_file(name_file + ".replace");
+	while (getline(cur_file, line_in_file))
+	{
+		if (argv[2][0] == '\0')
+			final_file << line_in_file << std::endl;
+		else
+		{	
+			poxvac_line = foo(line_in_file, argv[2], argv[3]);
+			final_file << poxvac_line << std::endl;
+		}
+	}
+	cur_file.close();
+	final_file.close();
+	return (0);
+}
