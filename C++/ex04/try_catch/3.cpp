5755fe4212ed8ab692e295f9b6932f410946d9b4
Vardan Barseghyan <vbarsegh@c2r1s2.42yerevan.am>
2024-12-22 23:17:38 +0400

    31 prc(try catch part 1)

--- C++/ex04/try_catch/3.cpp
+++ C++/ex04/try_catch/3.cpp
@@ -3,15 +3,27 @@
 int paragic_triangle(int a, int b, int c)
 {
     try
-    {
+    {   
         if (a < 0 || b < 0 || c < 0)
             throw -1;
     }
+    // catch(int x)
+    // {
+    //     std::cout << x << std::endl;
+    //     return x;
+    // }orinak1
+    
+    // catch(double x)
+    // {
+    //     std::cout << x << std::endl;
+    //     return x;
+    // }orinak2
+
     catch(int x)
     {
         std::cout << x << std::endl;
-        return x;
-    }
+        throw;
+    }//orinak3
     if (a > b + c || b > a + c || c > a + c)
         throw "Error: a, b, c are not triangle lenghts";
     return (a + b + c);
@@ -19,19 +31,19 @@ int paragic_triangle(int a, int b, int c)
 
 int main()
 {
-    // try
-    // {
-    //     int p = paragic_triangle(-5,1,2);
-    //     std::cout << "cuyc talu hamar vor ete paragic_triangle-i mej throw-n ashxati el es cout-is chi hasnelu" << std::endl;
-    // }
-    // catch(const char* e)
-    // {
-    //     std::cout << e << std::endl;
-    // }
-    // catch(int)
-    // {
-    //     std::cout << "Lenght must be positive digitals" << std::endl;
-    // }
+    try
+    {
+        int p = paragic_triangle(-5,1,2);
+        std::cout << "cuyc talu hamar vor ete paragic_triangle-i mej throw-n ashxati el es cout-is chi hasnelu" << std::endl;
+    }
+    catch(const char* e)
+    {
+        std::cout << e << std::endl;
+    }
+    catch(int)
+    {
+        std::cout << "Lenght must be positive digitals" << std::endl;
+    }
 
 
     return (0);
