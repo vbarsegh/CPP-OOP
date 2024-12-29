d703dd95fce9c3d3b030608915bef6ab9f5a506f
Vardan Barseghyan <vbarsegh@c2r1s2.42yerevan.am>
2024-12-29 21:52:57 +0400

    ex00 done

--- C++/CPP05/ex00/main.cpp
+++ C++/CPP05/ex00/main.cpp
@@ -1,6 +1,55 @@
 #include "Bureaucrat.hpp"
 
 int main() {
+    //	Testing bureaucrat set too high grade
+{
+	std::cout << "\033[0;32m" << "Testing bureaucrat set too high grade" << "\033[0m" << std::endl;
+	try {
+		Bureaucrat ponch("Armenchik", -89);
+	}
+	catch (Bureaucrat::GradeTooHighException& e) {
+		std::cout << "\033[0;031m" << e.what() << "\033[0m" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooLowException& e) {
+		std::cout << "\033[0;031m" << e.what() << "\033[0m" << std::endl;
+	}
+}
+std::cout << '\n';
+///////
+//	Testing bureaucrat set too low grade
+{
+	std::cout << "\033[0;32m" << "Testing bureaucrat set too low grade" << "\033[0m" << std::endl;
+	try {
+		Bureaucrat ponch("Armenchik", 777);
+	}
+	catch (Bureaucrat::GradeTooHighException& e) {
+		std::cout << "\033[0;031m" << e.what() << "\033[0m" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooLowException& e) {
+		std::cout << "\033[0;031m" << e.what() << "\033[0m" << std::endl;
+	}
+}
+std::cout << '\n';
+//////////
+//	Testing bureaucrat normal grade
+{
+	std::cout << "\033[0;32m" << "Testing bureaucrat normal grade" << "\033[0m" << std::endl;
+	try {
+		Bureaucrat ponch("Armenchik", 75);
+		std::cout << ponch << std::endl;
+		ponch.decrement_grade();
+		std::cout << ponch << std::endl;
+		ponch.increment_grade();
+		std::cout << ponch << std::endl;
+	}
+	catch (Bureaucrat::GradeTooHighException& e) {
+		std::cout << "\033[0;031m" << e.what() << "\033[0m" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooLowException& e) {
+		std::cout << "\033[0;031m" << e.what() << "\033[0m" << std::endl;
+	}
+}//////////
+std::cout << std::endl;
     try {
         Bureaucrat b1("Alice", 2);
         std::cout << b1 << std::endl;
